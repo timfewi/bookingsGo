283155115d56885d6827dc7214b81f35381f313b
timwitter <kontakt@timwitter.at>
2024-03-16 14:43:21 +0100

    added Template images to project

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,8 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	filesServer := http.FileServer(http.Dir("../../static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", filesServer))
 	return mux
 }
